83f4c6ede3e16f5a7bac0baccc5d503e824e1fdb
Tocksman <theeolk@gmail.com>
2012-03-19 17:42:14 -0400

    Add a command alias 'sp'

--- src/com/betaforce/shardin/SimplePoll/SimplePoll.java
+++ src/com/betaforce/shardin/SimplePoll/SimplePoll.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimplePoll extends JavaPlugin {
-    private SimplePollExecutor myExecutor;
+    private SimplePollExecutor executor;
     List<Poll> polls = new ArrayList<Poll>();
     
     @Override
     public void onEnable() {
-        myExecutor = new SimplePollExecutor(this);
-        getCommand("simplepoll").setExecutor(myExecutor);
+        executor = new SimplePollExecutor(this);
+        getCommand("simplepoll").setExecutor(executor);
+        getCommand("sp").setExecutor(executor);
     }
     
     @Override
